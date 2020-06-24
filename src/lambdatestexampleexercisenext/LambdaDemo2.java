979707de7f05b2bbb713c6113a568328b66a636b
tomek <tomeksochacki@wp.pl>
2020-06-24 15:36:50 +0200

    Updae

--- src/lambdatestexampleexercisenext/LambdaDemo2.java
+++ src/lambdatestexampleexercisenext/LambdaDemo2.java
@@ -9,6 +9,24 @@ public class LambdaDemo2 {
         if (isFactor.test(10, 3));{
             System.out.println("Number 3 isn't factor number 10.");
         }
+        System.out.println();
+
+        NumericTest lessThan = (n, m) -> (n < m);
+        if (lessThan.test(2, 10));{
+            System.out.println("Number 2 is less than number 10.");
+        };
+        if (!lessThan.test(10, 2));{
+            System.out.println("Number 10 isn't less than number 2.");
+        }
+        System.out.println();
+
+        NumericTest abcEquals = (n, m) -> (n < 0 ? -n : n) == (m < 0 ? -m : m);
+        if (abcEquals.test(4, -4));{
+            System.out.println("Absolute value 4 and -4 are equals.");
+        };
+        if (!abcEquals.test(4, -5));{
+            System.out.println("Absolute value 4 and -5 aren't equals.");
+        }
     }
 }
 
